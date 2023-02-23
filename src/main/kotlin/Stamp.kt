ffd0d281b322c9d61c2c50e6a5c2043dc35d87dd
Martín Zangl <ignaciozangl@gmail.com>
2023-02-23 10:52:08 -0300

    add backtracking

--- src/main/kotlin/Stamp.kt
+++ src/main/kotlin/Stamp.kt
@@ -52,25 +52,65 @@ fun exchangeWithMapList(
         return listOf(secondExchange, firstExchange)
     }
 
-    fun toFrequencyList(
-        array1: List<Int>,
-        array2: List<Int>
-    ): Pair<List<Int>, List<Int>> {
-        val size = max(array1.sorted().max(), array2.sorted().max())
-        val mapList1 = MutableList(size + 1) { 0 }
-        val mapList2 = MutableList(size + 1) { 0 }
-
-        array1.forEach {
-            mapList1[it] += 1
-        }
-
-        array2.forEach {
-            mapList2[it] += 1
-        }
-        return mapList1 to mapList2
-    }
-
     val (jane, alice) = toFrequencyList(janeStamps, aliceStamps)
 
     return compareAndExchange(jane, alice)
+}
+
+fun exchangeWithBacktracking(
+    janeStamps: List<Int>,
+    aliceStamps: List<Int>,
+    spare: Int = 2
+): List<List<Int>> {
+
+    val result = mutableListOf<List<Int>>()
+    val (jane, alice) = toFrequencyList(janeStamps, aliceStamps)
+
+    fun backtrack(
+        index: Int,
+        firstExchange: MutableList<Int> = mutableListOf<Int>(),
+        secondExchange: MutableList<Int> = mutableListOf<Int>()
+    ) {
+
+        if (index == alice.size) {
+            result.add(secondExchange.toList())
+            result.add(firstExchange.toList())
+            return
+        }
+
+        if (alice[index] < spare && jane[index] > spare) {
+            repeat(jane[index] - spare) {
+                firstExchange.add(index)
+            }
+        } else if (alice[index] > spare && jane[index] < spare) {
+            repeat(alice[index] - spare) {
+                secondExchange.add(index)
+            }
+        }
+
+        backtrack(index + 1, firstExchange, secondExchange)
+        if (firstExchange.isNotEmpty()) firstExchange.removeLast()
+        if (secondExchange.isNotEmpty()) secondExchange.removeLast()
+    }
+
+    backtrack(0)
+    return result
+}
+
+private fun toFrequencyList(
+    array1: List<Int>,
+    array2: List<Int>
+): Pair<List<Int>, List<Int>> {
+    val size = max(array1.sorted().max(), array2.sorted().max())
+    val mapList1 = MutableList(size + 1) { 0 }
+    val mapList2 = MutableList(size + 1) { 0 }
+
+    array1.forEach {
+        mapList1[it] += 1
+    }
+
+    array2.forEach {
+        mapList2[it] += 1
+    }
+    return mapList1 to mapList2
 }
--- src/test/kotlin/StampTest.kt
+++ src/test/kotlin/StampTest.kt
@@ -68,4 +68,37 @@ class StampTest {
             exchange
         )
     }
+
+    @Test
+    fun testExchangeBacktrackCase1() {
+        val expected = listOf(listOf(2, 3), listOf(1, 1, 7))
+        val exchange = exchangeWithBacktracking(listOf(1, 7, 3, 1, 7, 4, 5, 1, 7, 1), listOf(2, 3, 3, 2, 4, 3, 2))
+
+        Assertions.assertIterableEquals(
+            expected,
+            exchange
+        )
+    }
+
+    @Test
+    fun testExchangeBacktrackCase2() {
+        val expected = listOf<List<Int>>(emptyList(), emptyList())
+        val exchange = exchangeWithBacktracking(listOf(1, 2, 3, 4, 4), listOf(4, 4, 4, 5, 6, 7))
+
+        Assertions.assertIterableEquals(
+            expected,
+            exchange
+        )
+    }
+
+    @Test
+    fun testExchangeBacktrackCase3() {
+        val expected = listOf(listOf(), listOf(3, 3))
+        val exchange = exchangeWithBacktracking(listOf(5, 4, 4, 3, 3, 3, 3), listOf(1, 3))
+
+        Assertions.assertIterableEquals(
+            expected,
+            exchange
+        )
+    }
 }
