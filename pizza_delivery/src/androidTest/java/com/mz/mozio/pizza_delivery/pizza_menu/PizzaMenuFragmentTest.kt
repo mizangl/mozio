a7e33e7d6a108f37ad2411f9210d2a02a0d27693
Martín Ignacio Zangl <ignaciozangl@gmail.com>
2023-03-03 20:22:06 -0300

    Fix/remove barista (#15)

--- pizza_delivery/src/androidTest/java/com/mz/mozio/pizza_delivery/pizza_menu/PizzaMenuFragmentTest.kt
+++ pizza_delivery/src/androidTest/java/com/mz/mozio/pizza_delivery/pizza_menu/PizzaMenuFragmentTest.kt
@@ -1,14 +1,22 @@
 package com.mz.mozio.pizza_delivery.pizza_menu
 
-import com.adevinta.android.barista.assertion.BaristaRecyclerViewAssertions.assertRecyclerViewItemCount
-import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertContains
-import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
-import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotDisplayed
+import android.content.Context
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.internal.runner.junit4.statement.UiThreadStatement
+import com.google.gson.Gson
 import com.mz.mozio.pizza_delivery.R
 import com.mz.mozio.pizza_delivery.data.FakeDataProvider
 import com.mz.mozio.pizza_delivery.data.entity.PizzaDTO
 import com.mz.mozio.pizza_delivery.pizza_menu.view.PizzaMenuFragment
+import com.mz.mozio.pizza_delivery.utils.hasTextOn
+import com.mz.mozio.pizza_delivery.utils.isDisplayedOn
 import com.mz.mozio.pizza_delivery.utils.launchFragmentInHiltContainer
+import com.mz.mozio.pizza_delivery.utils.viewIsDisplayed
+import com.mz.mozio.pizza_delivery.utils.viewIsNotDisplayed
+import com.mz.mozio.pizza_delivery.utils.viewWith
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
@@ -17,6 +25,14 @@ import org.junit.Test
 
 @HiltAndroidTest
 class PizzaMenuFragmentTest {
+
+    private val context: Context = ApplicationProvider.getApplicationContext()
+    private val testNavController = TestNavHostController(context).apply {
+        UiThreadStatement.runOnUiThread {
+            setGraph(R.navigation.navigation_pizza)
+            setCurrentDestination(R.id.dialog_pizza_confirmation)
+        }
+    }
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
@@ -34,12 +50,12 @@ class PizzaMenuFragmentTest {
 
         launch()
 
-        assertDisplayed(R.id.error_image)
-        assertDisplayed(R.id.error_message)
-        assertDisplayed(R.id.error_retry)
+        R.id.error_image.viewIsDisplayed()
+        R.id.error_message.viewIsDisplayed()
+        R.id.error_retry.viewIsDisplayed()
 
-        assertContains(R.id.error_message, "Something went wrong!")
-        assertContains(R.id.error_retry, "Try again")
+        R.id.error_message.viewWith("Something went wrong!")
+        R.id.error_retry.viewWith("Try again")
     }
 
     @Test
@@ -56,17 +72,39 @@ class PizzaMenuFragmentTest {
 
         launch()
 
-        assertNotDisplayed(R.id.error_image)
-        assertNotDisplayed(R.id.error_message)
-        assertNotDisplayed(R.id.error_retry)
+        R.id.error_image.viewIsNotDisplayed()
+        R.id.error_message.viewIsNotDisplayed()
+        R.id.error_retry.viewIsNotDisplayed()
 
-        assertRecyclerViewItemCount(
-            R.id.pizza_menu,
-            2
-        )
+        checkPizza("Mozzarella", 0)
+        checkPizza("Pepperoni", 1)
+    }
+
+    private fun checkPizza(
+        name: String,
+        index: Int
+    ) {
+
+        R.id.image.isDisplayedOn(R.id.pizza_menu, index)
+        R.id.name.hasTextOn(R.id.pizza_menu, index, name)
+        R.id.action_complete.isDisplayedOn(R.id.pizza_menu, index)
+        R.id.action_half.isDisplayedOn(R.id.pizza_menu, index)
     }
 
     private fun launch() {
-        launchFragmentInHiltContainer<PizzaMenuFragment>()
+        launchFragmentInHiltContainer<PizzaMenuFragment>() {
+            lifecycleScope.launchWhenStarted {
+                if (view != null) {
+                    Navigation.setViewNavController(view!!, testNavController)
+                }
+            }
+        }
+    }
+
+    private fun loadPizzasFromJson(): List<PizzaDTO> {
+        val raw =
+            context.resources.openRawResource(R.raw.pizza_list)
+                .bufferedReader().use { it.readText() }
+        return Gson().fromJson(raw, Array<PizzaDTO>::class.java).toList()
     }
 }
---
+++ pizza_delivery/src/androidTest/java/com/mz/mozio/pizza_delivery/utils/Matchers.kt
@@ -0,0 +1,110 @@
+package com.mz.mozio.pizza_delivery.utils
+
+import android.view.View
+import androidx.annotation.IdRes
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsNot
+
+class RecyclerViewMatcher(@IdRes private val recyclerViewId: Int) {
+
+    companion object {
+        private const val INVALID_ID = -1
+
+        fun recyclerViewWithId(@IdRes viewId: Int): RecyclerViewMatcher {
+            return RecyclerViewMatcher(viewId)
+        }
+    }
+
+
+    fun itemViewAtIndex(index: Int): Matcher<View> {
+        return viewHolderViewAtPosition(index, INVALID_ID)
+    }
+
+    fun viewHolderViewAtPosition(index: Int, @IdRes targetViewId: Int): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            var itemView: View? = null
+
+            override fun describeTo(description: Description) {
+                description.appendText("RecyclerView view with id: $recyclerViewId not found.")
+            }
+
+            override fun matchesSafely(view: View): Boolean {
+                if (itemView == null) {
+                    val recyclerView: RecyclerView? =
+                        view.rootView.findViewById(recyclerViewId) as? RecyclerView
+                    if (recyclerView != null && recyclerView.id == recyclerViewId) {
+                        itemView = recyclerView.findViewHolderForAdapterPosition(index)!!.itemView
+                    } else {
+                        return false
+                    }
+                }
+
+                return if (targetViewId == INVALID_ID) {
+                    view === itemView
+                } else {
+                    val targetView = itemView!!.findViewById<View>(targetViewId)
+                    view === targetView
+                }
+            }
+        }
+    }
+}
+
+internal fun matchView(matcher: Matcher<View>): ViewInteraction = Espresso.onView(matcher)
+
+internal fun @receiver:IdRes Int.withId() = ViewMatchers.withId(this)
+internal fun @receiver:IdRes Int.matchView(): ViewInteraction = matchView(this.withId())
+
+internal fun @receiver:IdRes Int.viewIsDisplayed(): ViewInteraction =
+    matchView(this.withId()).check(matches(isDisplayed()))
+
+internal fun @receiver:IdRes Int.viewIsNotDisplayed(): ViewInteraction =
+    matchView(this.withId()).check(matches(IsNot.not(isDisplayed())))
+
+internal fun @receiver:IdRes Int.viewWith(text: String): ViewInteraction =
+    matchView(this.withId()).check(withText(text))
+
+internal fun @receiver:IdRes Int.clickOn(): ViewInteraction =
+    matchView().perform(ViewActions.click())
+
+internal fun @receiver:IdRes Int.isDisplayedInParent(@IdRes id: Int): ViewInteraction =
+    matchView(allOf(this.withId(), withParent(id.withId()))).check(matches(isDisplayed()))
+
+internal fun @receiver:IdRes Int.viewInParentWith(
+    @IdRes id: Int, text: String
+): ViewInteraction = matchView(allOf(this.withId(), withParent(id.withId()))).check(withText(text))
+
+internal fun <T : RecyclerView.ViewHolder> Int.performActionOnRecyclerViewItemAt(
+    index: Int, action: ViewAction
+) = matchView().performActionOnRecyclerViewItemAt<T>(index, action)
+
+internal fun @receiver:IdRes Int.asRecyclerView() = RecyclerViewMatcher.recyclerViewWithId(this)
+
+internal fun @receiver:IdRes Int.onRecyclerView(parent: Int, index: Int) =
+    matchView(parent.asRecyclerView().viewHolderViewAtPosition(index, this))
+
+internal fun @receiver:IdRes Int.isDisplayedOn(parent: Int, index: Int) =
+    this.onRecyclerView(parent, index).check(matches(isDisplayed()))
+
+internal fun @receiver:IdRes Int.hasTextOn(parent: Int, index: Int, text: String) =
+    this.onRecyclerView(parent, index).check(withText(text))
+
+fun <T : RecyclerView.ViewHolder> ViewInteraction.performActionOnRecyclerViewItemAt(
+    index: Int, action: ViewAction
+) = perform(RecyclerViewActions.actionOnItemAtPosition<T>(index, action))
+
+private fun withText(text: String) = matches(ViewMatchers.withText(text))
